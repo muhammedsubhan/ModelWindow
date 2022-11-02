492ad0987b924d9fe6c80f6ef842df8173c42215
muhammedsubhan <muhammedsubhan692@gmail.com>
2022-11-02 18:35:14 +0500

    Buttons/Add and Remove Classes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnOpenModel = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModel);
+
+const openModel = function () {
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModel = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener('click', openModel);
+}
+
+btnCloseModel.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
